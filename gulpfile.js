877c1e677565f3b0430c2ac939ea34ee61b5939f
Musalaeva Larisa <musssss.94@mail.ru>
2018-11-26 22:30:24 +0300

    выпилила node_modules, добавила таск deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-sass = require('gulp-sass');
+sass = require('gulp-sass'),
+
 
 gulp.task('sass', function() {
 	return gulp.src('app/sass/**/*.sass')
@@ -9,4 +10,12 @@ gulp.task('sass', function() {
 
 gulp.task('watch', function() {
 	gulp.watch('app/sass/**/*.sass', ['sass']);
+});
+
+gulp.task('deploy', function() {
+  return gulp.src(dirs.build + '/**/*')
+    .pipe(ghPages({
+      // origin: "upstream"
+      // remoteUrl:
+    }));
 });
